8527174c850a072a66df42b4a2deb2ee691656e9
Aaron Campf <aaroncampf@gmail.com>
2016-05-20 12:34:26 -0700

    sfsf

---
+++ LearningPython1/LearningPython1/PythonApplication1.py
@@ -0,0 +1,16 @@
+#class Company(object):
+#    self.
+
+"""Documentation"""
+class Company(object):
+    def __init__():
+        self.Name = ""
+
+
+
+
+AJP = Cp
+AJP.Nam
+AJP. = "Bacon"
+
+print("hello")
