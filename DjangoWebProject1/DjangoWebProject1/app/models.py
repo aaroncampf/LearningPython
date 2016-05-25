61bfc1fb6b091fd85a439d6a21d6ec87b84f2624
Aaron Campf <aaroncampf@gmail.com>
2016-05-25 13:01:42 -0700

    Screw you Django 1.9

---
+++ DjangoWebProject1/DjangoWebProject1/app/models.py
@@ -0,0 +1,29 @@
+﻿"""
+Definition of models.
+"""
+
+from django.db import models
+from django.db.models.query import QuerySet
+
+# Create your models here.
+class Person(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    Address = models.CharField(max_length=30)
+
+
+
+def Seed():
+    Test = QuerySet
+    Test = Person.objects
+    for x in Test.all():
+        pass
+
+    Aaron = Person()
+    Aaron.first_name = "Aaron"
+    Aaron.last_name = "Campf"
+    Aaron.save()
+
+
+#Seed()
---
+++ DjangoWebProject1/DjangoWebProject1/app/views.py
@@ -0,0 +1,80 @@
+﻿"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+from app.models import Person
+from django.db.models.query import QuerySet
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        })
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        })
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        })
+    )
+
+def Test(request, id):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+
+    #from django.core.management import call_command
+    #call_command('migrate')
+    #call_command('makemigrations', 'myapp', verbosity=3, interactive=False)
+    #call_command('migrate', 'myapp', verbosity=3, interactive=False)
+
+    Aaron = Person()
+    Aaron.first_name = "Aaron"
+    Test = QuerySet
+    Test = Person.objects
+    for x in Test.all():
+        pass
+
+
+    return render(
+        request,
+        'app/Test.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+            'Person': Aaron
+        })
+    )
